9b20f3323439e9db40569b4d3d65c4734e8162e3
adrian <thechouzanone@gmail.com>
2019-03-31 21:08:38 -0700

    Another version, maybe simplier but not functional

--- bebop_coordinates/scripts/CoordinateSystem.py
+++ bebop_coordinates/scripts/CoordinateSystem.py
@@ -13,57 +13,44 @@ class CoordinateSystem:
     def __init__(self, speed=0.5, turnSpeed=1):
         self.position = None
         self.theta = None
-        self.firstTheta = None
-        self.firstPosition = None
+        self.velocity = None
         self.odomSubs = rospy.Subscriber('odom', Odometry, self.updatePosition)
-        self.rate = rospy.Rate(60)
+
+        self.frequency = 5
+        self.rate = rospy.Rate(self.frequency)
+        
         self.pose = np.asarray([0,0,0,0])
         self.speed = speed
         self.turnSpeed = turnSpeed
+        
         self.state = "LAND"
+        
         self.posePub = rospy.Publisher('cmd_vel', Twist, queue_size=1) 
         self.takeoffPub = rospy.Publisher('takeoff', Empty, queue_size=1)
         self.flattrimPub = rospy.Publisher('flattrim', Empty, queue_size=1)
         self.landPub = rospy.Publisher('land', Empty, queue_size=1)
+        
         self.empty_msg = Empty()
         self.twist = Twist()
-        self.CMD_VEL_THREAD = threading.Thread(target=self.publishTwist)
-        self.CMD_VEL_THREAD.setDaemon(True)
-        self.CMD_VEL_THREAD.start()
-        self.first = True
 
     def updatePosition(self, data):
-        pos = data.pose.pose.position
-        theta = data.pose.pose.orientation.z
-        newPosition = np.asarray([pos.x,pos.y,pos.z])
-        if self.first:
-            self.firstTheta = theta
-            self.firstPosition = newPosition.copy()
-            self.first = False
-        else:
-            newPosition -= self.firstPosition
-            newTheta = theta - self.firstTheta
-            self.theta = newTheta
-            self.position = newPosition.copy()
+        position = data.pose.pose.position
+        orientation = data.pose.pose.orientation
+        linear = data.twist.twist.linear
+        angular = data.twist.twist.angular
+
+        self.position = np.array([position.x, position.y, position.z])
+        self.orientation = np.array([orientation.x,orientation.y,orientation.z])
+        self.velocity = np.array([linear.x,linear.y,linear.z,angular.z])
 
     def publishTwist(self):
-        try:
-            while not rospy.is_shutdown():
-                if(self.state == "AIR"):
-                    # rospy.loginfo("Publishing pose: %s"%(self.pose))
-                    self.twist.linear.x = self.pose[0]
-                    self.twist.linear.y = self.pose[1]
-                    self.twist.linear.z = self.pose[2]
-                    self.twist.angular.x = 0
-                    self.twist.angular.y = 0
-                    self.twist.angular.z = self.pose[3]
-                    self.posePub.publish(self.twist)
-                self.rate.sleep()
-        except (KeyboardInterrupt, SystemExit) as e:
-            print(e)
-        finally:
-            self.twist.linear.x = 0; self.twist.linear.y = 0; self.twist.linear.z = 0
-            self.twist.angular.x = 0; self.twist.angular.y = 0; self.twist.angular.z = 0
+        if(self.state == "AIR"):
+            self.twist.linear.x = self.pose[0]
+            self.twist.linear.y = self.pose[1]
+            self.twist.linear.z = self.pose[2]
+            self.twist.angular.x = 0
+            self.twist.angular.y = 0
+            self.twist.angular.z = self.pose[3]
             self.posePub.publish(self.twist)
 
     def RotMat(self):
@@ -75,49 +62,55 @@ class CoordinateSystem:
         ])
 
     def moveTo(self, destiny): #NEEDS TO IMPLEMENT ROTATION
-        try:
-            destiny = np.asarray(destiny)
-            print("I am at %s"%self.position)
-            print("Moving to %s"%destiny)
-            error = np.linalg.norm(destiny-self.position)
-            counter = 1
-            while (error > 0.1):
-                if counter%5==0:
-                    print("Destiny: %s"%destiny)
-                    print("Position: %s"%self.position)
-                    print("Disntace: %s"%error)
-                    counter = 0
-                    newPose = np.asarray([0,0,0,0])
-                    rate.sleep()
-                else:
-                    direction = destiny - self.position
-                    distance = np.linalg.norm(direction)
-                    t = distance / self.speed
-                    velocity = direction/t
-                    newPose = np.asarray([
-                        velocity[0],
-                        velocity[1],
-                        velocity[2],
-                        self.pose[3]
-                    ])
-                self.pose = newPose.copy()
-                error = np.linalg.norm(destiny-self.position)
-                counter+=1
+        destiny = np.asarray(destiny)
+        error = np.linalg.norm(destiny-self.position)
+        counter = 1
+        print("Destiny: %s"%destiny)
+        print("Position: %s"%self.position)
+        print("Distance: %s"%error)
+        while (error > 0.1):
+            if counter%2==0:
+                print("Destiny: %s"%destiny)
+                print("Position: %s"%self.position)
+                print("Pose: %s"%self.pose)
+                print()
+                counter = 0
+                newPose = np.array([0,0,0,0])
                 self.rate.sleep()
-            self.pose[0] = 0
-            self.pose[1] = 0
-            self.pose[2] = 0
-            print("Arrived to destiny") 
-        except:
-            self.pose[0] = 0
-            self.pose[1] = 0
-            self.pose[2] = 0
+            else:
+                direction = destiny - self.position
+                distance = np.linalg.norm(direction)
+                t = distance / self.speed
+                velocity = direction/t
+                newPose = np.asarray([
+                    velocity[0],
+                    velocity[1],
+                    velocity[2],
+                    0
+                ])
+                
+            self.pose = newPose.copy()
+            print("Distance: %s"%error)
+            self.publishTwist()
+            error = np.linalg.norm(destiny-self.position)
+            counter+=1
+            self.rate.sleep()
+
+        self.brake()
+        print("Arrived to destiny") 
+
+    def brake(self, sleepTime=True):
+        newPose = np.asarray([0,0,0,0])
+        self.publishTwist()
+        if sleepTime:
+            sleep(2)
+
     def takeoff(self):
         self.takeoffPub.publish(self.empty_msg)
-        sleep(5)
+        sleep(8)
         self.first = True
         self.state = "AIR"
-    
+
     def land(self):
         self.landPub.publish(self.empty_msg)
         self.state = "LAND"
