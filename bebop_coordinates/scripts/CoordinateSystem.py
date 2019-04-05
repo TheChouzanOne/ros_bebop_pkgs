d1b193874c7724fbaccd6d30f6cec0b82691934d
adrian <thechouzanone@gmail.com>
2019-04-05 15:59:12 -0700

    Modified PID behavior. Now it always tries to stabilize at its destiny.

--- bebop_coordinates/scripts/CoordinateSystem.py
+++ bebop_coordinates/scripts/CoordinateSystem.py
@@ -13,15 +13,18 @@ import sys
 class CoordinateSystem:
     def __init__(self, speed=0.5, turnSpeed=1):
         self.position = None
+        self.destiny = None
         self.theta = None
         self.velocity = None
         self.odomSubs = rospy.Subscriber('odom', Odometry, self.updatePosition)
 
-        self.Kp = 0.8
-        self.Ki = 0.1
-        self.Kd = 0.5
 
-        self.frequency = 20
+        self.pid = list()
+        self.Kp = 0.4
+        self.Ki = 0
+        self.Kd = 1
+
+        self.frequency = 10
         self.rate = rospy.Rate(self.frequency)
         
         self.pose = np.asarray([0,0,0,0])
@@ -38,6 +41,10 @@ class CoordinateSystem:
         self.empty_msg = Empty()
         self.twist = Twist()
 
+        self.MoveThread = threading.Thread(target=self.PIDMove)
+        self.MoveThread.setDaemon(True)
+
+
     def updatePosition(self, data):
         position = data.pose.pose.position
         orientation = data.pose.pose.orientation
@@ -49,14 +56,13 @@ class CoordinateSystem:
         self.velocity = np.array([linear.x,linear.y,linear.z,angular.z])
 
     def publishTwist(self):
-        if(self.state == "AIR"):
-            self.twist.linear.x = self.pose[0]
-            self.twist.linear.y = self.pose[1]
-            self.twist.linear.z = self.pose[2]
-            self.twist.angular.x = 0
-            self.twist.angular.y = 0
-            self.twist.angular.z = self.pose[3]
-            self.posePub.publish(self.twist)
+        self.twist.linear.x = self.pose[0]
+        self.twist.linear.y = self.pose[1]
+        self.twist.linear.z = self.pose[2]
+        self.twist.angular.x = 0
+        self.twist.angular.y = 0
+        self.twist.angular.z = self.pose[3]
+        self.posePub.publish(self.twist)
 
     def RotMat(self):
         t = self.theta
@@ -66,37 +72,33 @@ class CoordinateSystem:
             [0        , 0         , 1]
         ])
 
-    def totalError(self, errorList):
-        error = 0
-        for err in errorList:
-            error += err*err
-        
-        return np.sqrt(error)
+    def PIDMove(self):
+        error = np.zeros(3)
+        while(True):
+            if("AIR"):
+                newPose = np.zeros(4)
+                for i in range(3):
+                    error[i] = newPose[i] = self.pid[i](self.position[i])            
+                self.pose = newPose.copy()
+                print("Velocity: %s"%np.linalg.norm(error))
+                self.publishTwist()
+                self.rate.sleep()
+            else:
+                pass
+
+    def setPIDDestiny(self, destiny):
+        for i in range(3):
+            self.pid[i].setpoint = destiny[i]
 
     def moveTo(self, destiny): #NEEDS TO IMPLEMENT ROTATION
         destiny = np.asarray(destiny)
-        pid = list()
-        error = list()
-        for i in range(3):
-            pid.append(PID(self.Kp, self.Ki, self.Kd, setpoint=destiny[i], output_limits=(-self.speed,self.speed)))
-            error.append(pid[i](self.position[i]))
-        
-        print("Destiny: %s"%destiny)
-        print("Position: %s"%self.position)
-        print("Initial velocity: %s"%self.totalError(error))
-        print("Distance: %s"%self.totalError(destiny-self.position))
-        while (self.totalError(error) > .03 or self.totalError(destiny-self.position) > 0.1):
-            newPose = np.zeros(4)
-            for i in range(3):
-                error[i] = newPose[i] = pid[i](self.position[i])            
-            self.pose = newPose.copy()
-            print("Velocity: %s"%self.totalError(error))
-            print("Distance: %s"%self.totalError(destiny-self.position))
-            print()
-            self.publishTwist()
+        self.rate.sleep()
+        self.setPIDDestiny(destiny)
+        err = np.linalg.norm(destiny-self.position)
+        while (err > 0.1):
             self.rate.sleep()
-
-        self.brake(sleepTime=False)
+            err = np.linalg.norm(destiny-self.position)
+            print("\nDistance: %s"%err)
         print("Arrived to destiny") 
 
     def brake(self, sleepTime=True):
@@ -108,12 +110,16 @@ class CoordinateSystem:
     def takeoff(self):
         self.takeoffPub.publish(self.empty_msg)
         sleep(8)
-        self.first = True
+        self.destiny = self.position.copy()
+        for i in range(3):
+            self.pid.append(PID(self.Kp, self.Ki, self.Kd, setpoint=self.destiny[i], output_limits=(-self.speed,self.speed)))
         self.state = "AIR"
+        self.MoveThread.start()
 
     def land(self):
-        self.landPub.publish(self.empty_msg)
         self.state = "LAND"
+        self.rate.sleep()
+        self.landPub.publish(self.empty_msg)
 
     def flattrim(self):
         self.flattrimPub.publish(self.empty_msg)
